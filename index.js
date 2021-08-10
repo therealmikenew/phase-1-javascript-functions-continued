e2b88ba311565c3120d01e58427f811a996069f8
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-10 17:07:09 -0400

    commit tree 55b2bdc27f4f1af143a823dafb398554fd2ac207

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity2="go to the office"){
+    return `This Monday, I will ${activity2}.`;
+}
+
+function wrapAdjective(string="*"){
+    return function(adj="special"){
+        return `You are ${string}${adj}${string}!`
+    }
+}
